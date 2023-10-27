a3c0b3929cbfaec4dd04f9954b8da805b79f64f5
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-10-27 17:24:12 +0200

    Initial commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { GranpLibModule } from 'granp-lib';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, GranpLibModule],
+})
+export class Tab1Page {
+  constructor() {}
+}
