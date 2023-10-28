aa7c0c2546add2cdf923f0acef1e431939f9434b
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-10-28 19:37:46 +0200

    Test Lib Component

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { GranpLibModule } from 'granp-lib';
+import { LoginButtonComponent } from 'granp-lib';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, GranpLibModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, LoginButtonComponent],
 })
 export class Tab1Page {
   constructor() {}
