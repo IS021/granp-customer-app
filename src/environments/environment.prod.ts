534b757a9e685d1a0ea4d54880155007dfb58a7b
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-10-29 10:27:17 +0100

    Added Imports for Auth0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,31 @@
 export const environment = {
-  production: true
-};
+    production: false,
+    auth0: {
+      domain: 'antopio.eu.auth0.com',
+      clientId: 'n9JNqSVKKWeaHp0bmBerwmfNWfKhSSaZ',
+      authorizationParams: {
+          redirect_uri: 'io.ionic.auth0.test://antopio.eu.auth0.com/capacitor/io.ionic.auth0.test/callback',
+          audience: 'https://hello-world.example.com',
+          // scope: '',
+      },
+  
+      
+      httpInterceptor: {
+          allowedList: [
+              {
+                  uri: 'http://192.168.1.110:6060 # http://localhost:6060 # Or http://10.0.2.2:6060 if you are running the Ionic Angular app on Android/api/messages/*',
+                  // tokenOptions: {
+                      // authorizationParams: {
+                          // audience: 'https://hello-world.example.com/api/v2/',
+                          // scope: '',
+                      // },
+                  // },
+              },
+          ],
+      },
+    },
+    granp: {
+      apiServerUrl: 'http://192.168.1.110:6060 # http://localhost:6060 # Or http://10.0.2.2:6060 if you are running the Ionic Angular app on Android',
+    },
+  };
+  
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,23 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { GranpLibModule, GranpLibConfig } from 'granp-lib';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes),
-  ],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        provideIonicAngular(),
+        provideRouter(routes),
+        importProvidersFrom(AuthModule.forRoot(environment.auth0)),
+        importProvidersFrom(GranpLibModule.forRoot(environment.granp)),
+    ],
 });
