da2d7b2781a99ec641defe06a55c71244af64dab
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-11-23 19:38:28 +0100

    Changed theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('granp-lib').then((m) => m.LoginPage),
+  }
 ];
