1422084d4007758c776960f8fcd84745aed3e579
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-11-24 17:48:03 +0100

    Added registration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,20 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('granp-lib').then((m) => m.LoginPage),
-  }
+    {
+        path: 'registration',
+        loadComponent: () => import('./registration/registration.page').then(m => m.RegistrationPage),
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('granp-lib').then((m) => m.LoginPage),
+    },
+    {
+        path: '',
+        loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    },
+    {
+        path: 'registration',
+        loadComponent: () => import('./registration/registration.page').then(m => m.RegistrationPage)
+    },
 ];
---
+++ src/app/registration/registration.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+    selector: 'app-registration',
+    templateUrl: './registration.page.html',
+    styleUrls: ['./registration.page.scss'],
+    standalone: true,
+    imports: [IonicModule, CommonModule, FormsModule]
+})
+export class RegistrationPage implements OnInit {
+
+    data = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    };
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+
+}
