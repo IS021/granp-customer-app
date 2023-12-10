8ef439449a0a69ef38da386b9f550394dbf5458b
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-12-10 22:08:37 +0100

    Added buttons to professional details

--- src/app/pages/professional-details/professional-details.page.ts
+++ src/app/pages/professional-details/professional-details.page.ts
@@ -2,23 +2,23 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
-  IonItem,
-  IonLabel,
-  IonList,
-  IonContent,
-  IonText,
-  IonRow, 
-  IonCol,
-  IonHeader,
-  IonToolbar,
-  IonBackButton,
-  IonButtons,
-  IonTitle,
-  IonGrid,
-  IonButton,
-  IonIcon
-  } 
-from '@ionic/angular/standalone'
+    IonItem,
+    IonLabel,
+    IonList,
+    IonContent,
+    IonText,
+    IonRow,
+    IonCol,
+    IonHeader,
+    IonToolbar,
+    IonBackButton,
+    IonButtons,
+    IonTitle,
+    IonGrid,
+    IonButton,
+    IonIcon
+}
+    from '@ionic/angular/standalone'
 
 import { addIcons } from 'ionicons';
 import { call, home, personCircleOutline, mailOpenOutline, calendarOutline, medicalOutline, locationOutline, walletOutline, checkmarkOutline, closeOutline, checkmarkDoneCircle, alertCircle, maleOutline, femaleOutline, helpOutline } from 'ionicons/icons';
@@ -29,64 +29,84 @@ import { AvatarComponent } from 'granp-lib';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-professional-details',
-  templateUrl: './professional-details.page.html',
-  styleUrls: ['./professional-details.page.scss'],
-  standalone: true,
-  imports: [CommonModule, 
-    FormsModule, 
-    IonItem, 
-    IonLabel, 
-    IonList, 
-    IonContent, 
-    IonText, 
-    IonRow, 
-    IonCol, 
-    IonHeader,
-    IonToolbar,
-    IonBackButton,
-    IonButtons,
-    IonTitle,
-    IonGrid,
-    IonButton,
-    IonIcon,
-    AvatarComponent
-  ]
+    selector: 'app-professional-details',
+    templateUrl: './professional-details.page.html',
+    styleUrls: ['./professional-details.page.scss'],
+    standalone: true,
+    imports: [CommonModule,
+        FormsModule,
+        IonItem,
+        IonLabel,
+        IonList,
+        IonContent,
+        IonText,
+        IonRow,
+        IonCol,
+        IonHeader,
+        IonToolbar,
+        IonBackButton,
+        IonButtons,
+        IonTitle,
+        IonGrid,
+        IonButton,
+        IonIcon,
+        AvatarComponent
+    ]
 })
 export class ProfessionalDetailsPage {
 
-  activatedRoute = inject(ActivatedRoute);
+    activatedRoute = inject(ActivatedRoute);
 
-  professional?: ProfessionalPublicResponse;
+    professional?: ProfessionalPublicResponse;
 
-  constructor() {
-    addIcons({ call, home, personCircleOutline, mailOpenOutline, calendarOutline, medicalOutline, locationOutline, walletOutline, checkmarkOutline, closeOutline, checkmarkDoneCircle, alertCircle,  maleOutline, femaleOutline, helpOutline });
+    showButtons: boolean = false;
 
-    this.activatedRoute.queryParams.subscribe(params => {
-      this.professional = JSON.parse(params["professional"]) as ProfessionalPublicResponse;
-    });
-  }
+    constructor() {
+        addIcons({ call, home, personCircleOutline, mailOpenOutline, calendarOutline, medicalOutline, locationOutline, walletOutline, checkmarkOutline, closeOutline, checkmarkDoneCircle, alertCircle, maleOutline, femaleOutline, helpOutline });
 
-  checkGender(): string {
-    switch (this.professional?.gender) {
-      case Gender.Male:
-        return 'Uomo';
-      case Gender.Female:
-        return 'Donna';
-      default:
-        return 'Non definito';
+        this.activatedRoute.queryParams.subscribe(params => {
+            this.professional = JSON.parse(params["professional"]) as ProfessionalPublicResponse;
+            this.showButtons = params["showButtons"] == "true";
+        });
     }
-  }
 
-  checkGenderIcon(): string {
-    switch (this.professional?.gender) {
-      case Gender.Male:
-        return 'male-outline';
-      case Gender.Female:
-        return 'female-outline';
-      default:
-        return 'help-outline';
+    getProfession(profession?: Profession) {
+        switch (profession) {
+            case Profession.Doctor:
+                return "Dottore";
+            case Profession.Nurse:
+                return "Infermiere";
+            case Profession.Physiotherapist:
+                return "Fisioterapista";
+            case Profession.Caregiver:
+                return "Assistente";
+            case Profession.Other:
+                return "Altro";
+            default:
+                return "Nessuna professione";
+        }
     }
-  }
+
+    checkGender(): string {
+        switch (this.professional?.gender) {
+            case Gender.Male:
+                return 'Uomo';
+            case Gender.Female:
+                return 'Donna';
+            default:
+                return 'Non definito';
+        }
+    }
+
+    checkGenderIcon(): string {
+        switch (this.professional?.gender) {
+            case Gender.Male:
+                return 'male-outline';
+            case Gender.Female:
+                return 'female-outline';
+            default:
+                return 'help-outline';
+        }
+    }
 
 }
