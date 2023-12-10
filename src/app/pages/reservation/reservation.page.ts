91db40e6c17154220a8843a0a861d5c5e315e8eb
MariarosaMarchese <mariarosamarchese2@gmail.com>
2023-12-10 21:45:31 +0100

    Added reservation page

--- src/app/pages/reservation/reservation.page.ts
+++ src/app/pages/reservation/reservation.page.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonDatetime, IonModal, IonInput } from '@ionic/angular/standalone';
+import { ReservationRequest } from 'granp-lib';
+
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.page.html',
   styleUrls: ['./reservation.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonDatetime, IonModal, IonInput ]
 })
 export class ReservationPage implements OnInit {
+
+  reservationRequest?: ReservationRequest;
 
   constructor() { }
 
