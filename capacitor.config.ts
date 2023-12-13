0c18e47d85d18a2e0a4a593dae989d70a1cc1bd9
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-12-13 18:16:38 +0100

    Added splash and fixed ios bug

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'granp.app.customer',
-  appName: 'granp-customer-app',
+  appName: 'GranP',
   webDir: 'www',
   server: {
     androidScheme: 'https'
